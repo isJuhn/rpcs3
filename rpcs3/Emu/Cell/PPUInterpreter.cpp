4f8c913c5b77b612276305a37fa4ca12b697b693
isJuhn <johanhellgren.98@gmail.com>
2021-03-28 22:17:47 +0200

    Stuff

--- rpcs3/Emu/Cell/PPUInterpreter.cpp
+++ rpcs3/Emu/Cell/PPUInterpreter.cpp
@@ -522,6 +522,35 @@ auto ppu_feed_data(ppu_thread& ppu, u64 addr)
 	return value;
 }
 
+bool visit_internal(ppu_thread& ppu, u32 curr, u32 level, u32 maxlevel)
+{
+	if (curr < 0x166BE87)
+		return true;
+	if (!ptr_map.contains(curr) || level >= maxlevel)
+		return false;
+	auto base = ptr_map[curr];
+	bool ret = false;
+	for (std::pair<u32, ptr_info> ptr_info : base)
+	{
+		if (ptr_info.first != curr && vm::check_addr(ptr_info.first + ptr_info.second.offset) && ppu_feed_data<u32>(ppu, ptr_info.first + ptr_info.second.offset) == curr && visit_internal(ppu, ptr_info.first, level + 1, maxlevel))
+		{
+			ppu_int.error("result: 0x%x, from: 0x%x + 0x%x, at 0x%x", curr, ptr_info.first, ptr_info.second.offset, ptr_info.second.cia);
+			return true;
+		}
+	}
+	return ret;
+};
+
+bool visit_ptr_map(ppu_thread& ppu, u32 curr)
+{
+	for (int i = 1; i < 12; i++)
+	{
+		if (visit_internal(ppu, curr, 0, i))
+			return true;
+	}
+	return false;
+}
+
 // Push called address to custom call history for debugging
 template <ppu_exec_bit... Flags>
 u32 ppu_record_call(ppu_thread& ppu, u32 new_cia, ppu_opcode_t op, bool indirect = false)
@@ -4586,11 +4615,11 @@ auto STVX()
 	const u64 addr = (op.ra ? ppu.gpr[op.ra] + ppu.gpr[op.rb] : ppu.gpr[op.rb]) & ~0xfull;
 	PPU_WRITE(v128, vm::cast(addr), ppu.vr[op.vs]);
 	// Put this piece of code in the correct instruction that writes a camera value
-	/*if (ppu.cia == 0x23C520) // Enter the cia for the instruction that writes a camera value
+	/*if (ppu.cia == 0x795388) // Enter the cia for the instruction that writes a camera value
 	{
 		std::function<bool(u32, u32)> visit = [&](u32 curr, u32 level) -> bool
 		{
-			if (curr < 0x11E8F68) // Enter the elf size, e.g. the last address you can see in IDA/Ghidra
+			if (curr < 0x15F58F8) // Enter the elf size, e.g. the last address you can see in IDA/Ghidra
 				return true;
 			if (!ptr_map.contains(curr) || level > 16)
 				return false;
@@ -4606,8 +4635,8 @@ auto STVX()
 			}
 			return ret;
 		};
-		visit(ppu.gpr[27], 0); // Enter the ppu register which contains the base address on the camera value, in this case r27
-		ppu_int.fatal("cia: 0x%x, lr: 0x%x, ctr: 0x%x, r5: 0x%x, r27: 0x%x", ppu.cia, ppu.lr, ppu.ctr, ppu.gpr[5], ppu.gpr[27]); // Log important stuff like base address and offset of camera value
+		visit(ppu.gpr[31], 0); // Enter the ppu register which contains the base address on the camera value, in this case r27
+		ppu_int.fatal("cia: 0x%x, lr: 0x%x, ctr: 0x%x, r5: 0x%x, r27: 0x%x", ppu.cia, ppu.lr, ppu.ctr, ppu.gpr[5], ppu.gpr[31]); // Log important stuff like base address and offset of camera value
 	}*/
 	};
 	RETURN_(ppu, op);
@@ -5808,7 +5837,7 @@ auto LWZ()
 	const u64 addr = op.ra || 1 ? ppu.gpr[op.ra] + op.simm16 : op.simm16;
 	const u32 value = ppu_feed_data<u32, Flags...>(ppu, addr);
 	ppu.gpr[op.rd] = value;
-	/*if (value >= 0x11E8F68 && value < 0x50000000) // Enter size of elf and some arbitrary upper bound for base addresses, upper bound can be removed if you have no idea
+	/*if (value >= 0x166BE87 && value) // Enter size of elf and some arbitrary upper bound for base addresses, upper bound can be removed if you have no idea
 	{
 		std::unique_lock lock(ptr_mutex);
 		if (!ptr_map.contains(value))
@@ -6105,13 +6134,13 @@ auto STFS()
 	static const auto exec = [](ppu_thread& ppu, ppu_opcode_t op) {
 	const u64 addr = op.ra || 1 ? ppu.gpr[op.ra] + op.simm16 : op.simm16;
 	PPU_WRITE(f32, vm::cast(addr), static_cast<float>(ppu.fpr[op.frs]));
-	/*if (ppu.cia == 0x155028)
+	/*if (ppu.cia == 0x5B77B8)
 	{
 		std::function<bool(u32, u32)> visit = [&](u32 curr, u32 level) -> bool
 		{
-			if (curr < 0x11E8F68)
+			if (curr < 0x166BE87)
 				return true;
-			if (!ptr_map.contains(curr) || level > 16)
+			if (!ptr_map.contains(curr) || level > 12)
 				return false;
 			auto base = ptr_map[curr];
 			bool ret = false;
@@ -6125,8 +6154,13 @@ auto STFS()
 			}
 			return ret;
 		};
-		visit(ppu.gpr[3], 0);
-		ppu_int.fatal("cia: 0x%x, lr: 0x%x, ctr: 0x%x, r3: 0x%x", ppu.cia, ppu.lr, ppu.ctr, ppu.gpr[3]);
+		visit(ppu.gpr[31], 0);
+		ppu_int.fatal("cia: 0x%x, lr: 0x%x, ctr: 0x%x, r31: 0x%x", ppu.cia, ppu.lr, ppu.ctr, ppu.gpr[31]);
+	}*/
+	/*if (ppu.cia == 0x5B77B8)
+	{
+		visit_ptr_map(ppu, ppu.gpr[31]);
+		ppu_int.fatal("cia: 0x%x, lr: 0x%x, ctr: 0x%x, r31: 0x%x", ppu.cia, ppu.lr, ppu.ctr, ppu.gpr[31]);
 	}*/
 	};
 	RETURN_(ppu, op);
